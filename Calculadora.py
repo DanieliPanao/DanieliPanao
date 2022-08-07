eeccb6f7d0dde2e2f49b3646f7f266a340d2712c
danieli.panao <danielipanao@hotmail.com>
2022-08-06 23:39:58 -0300

    Criacao da operacao de elevacao ao cubo

--- Calculadora.py
+++ Calculadora.py
@@ -17,11 +17,15 @@ def divisao(a, b):
     return a / b
 
 
+def cubo(a):
+    return a * a * a
+
+
 print("Selecione o número da operação desejada:")
 
-print("1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão")
+print("1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão \n5 - Elevar ao Cubo")
 
-opcao = int(input("Digite sua opção (1/2/3/4): "))
+opcao = int(input("Digite sua opção (1/2/3/4/5): "))
 
 if opcao == 1:
     a = int(input("Digite o primeiro número: "))
@@ -39,5 +43,8 @@ elif opcao == 4:
     a = int(input("Digite o primeiro número para divisão: "))
     b = int(input("Digite o segundo número para divisão: "))
     print(a, '/', b, '=', divisao(a, b))
+elif opcao == 5:
+    a = int(input("Digite o número para ser elevado ao cubo: "))
+    print(a, '*', a, '*', a, '=', cubo(a))
 else:
     print('Opção Inválida, tente novamente!')
